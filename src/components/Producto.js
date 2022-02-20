e53e931e0e488e5fbbdfa6e8f918ff5a4b81137b
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-02-20 00:08:21 -0500

    Estilos a los productos

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,11 +1,25 @@
 import { Component } from "react";
 
+
+const styles = {
+    producto:{
+        border: 'solid 1px #eee',
+        boxShadow: '0 5px 5px rgb(0, 0, 0, 0.1)',
+        width: '30%',
+        padding: '10px 15px',
+        borderRadius: '5px',
+    },
+    img:{
+        width: '100%',
+    }
+}
+
 class Producto extends Component{   
     render(){
        //console.log(this.props)
         const {producto} = this.props
         return(
-            <div>
+            <div style={styles.producto}>
                 <img src={ producto.img} alt={producto.name} />
                 <h3>{producto.name}</h3>
                 <p>{producto.price}</p>
